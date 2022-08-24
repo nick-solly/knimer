e4f75a93d4fef9d3803429ad6df160220c2c6632
Nick Solly <nick.solly@quickrelease.co.uk>
2022-08-24 14:49:51 -0500

    feat: add Dockerfile and Makefile

---
+++ docker/startup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+S3_LOCATION="s3://${S3_BUCKET_NAME}/${KNIME_WORKFLOW_FILE}.zip"
+
+aws s3 cp ${S3_LOCATION} /tmp/workflow.zip
+
+/opt/knime_4.6.0/knime -reset -nosave -consoleLog -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile=/tmp/workflow.zip
