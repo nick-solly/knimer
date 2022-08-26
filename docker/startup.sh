c4b3e0af42d0ea3d393d4c263b6aeffe2068abe4
Nick Solly <nick.solly@quickrelease.co.uk>
2022-08-26 14:43:17 -0500

    feat: handle workflow variables and secrets

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,4 +6,7 @@ S3_LOCATION="s3://${S3_BUCKET_NAME}/${KNIME_WORKFLOW_FILE}.zip"
 
 aws s3 cp ${S3_LOCATION} /tmp/workflow.zip
 
-/opt/knime_4.6.0/knime -reset -nosave -consoleLog -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile=/tmp/workflow.zip
+wfvars=( $WORKFLOW_VARIABLES )
+wfsecrets=( $WORKFLOW_SECRETS )
+
+/opt/knime_4.6.0/knime -reset -nosave -consoleLog -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile=/tmp/workflow.zip "${wfvars[@]}" "${wfsecrets[@]}"
