187202c1b926041f34e2212baff0524a1a5339a6
Nick Solly <nick.solly@quickrelease.co.uk>
2022-09-25 18:37:30 -0500

    feat: add cluster arn output

--- terraform/knimer/outputs.tf
+++ terraform/knimer/outputs.tf
@@ -1,3 +1,8 @@
+output "cluster_arn" {
+  value       = module.ecs.cluster_arn
+  description = "ARN of the ECS cluster"
+}
+
 output "task_definition_arn" {
   value       = module.ecs.task_definition_arn
   description = "ARN of the Task Definition"
