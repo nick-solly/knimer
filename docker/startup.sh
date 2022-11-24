ad18e2dd94f25241b83f16225a7deae2b61a5c9d
Nick Solly <nick@sudofox.co>
2022-11-23 21:17:32 -0600

    build: update KNIME version to 4.6.4

--- docker/startup.sh
+++ docker/startup.sh
@@ -9,4 +9,4 @@ aws s3 cp ${S3_LOCATION} /tmp/workflow.zip
 wfvars=( $WORKFLOW_VARIABLES )
 wfsecrets=( $WORKFLOW_SECRETS )
 
-/opt/knime_4.6.1/knime -reset -nosave -consoleLog -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile=/tmp/workflow.zip "${wfvars[@]}" "${wfsecrets[@]}"
+/opt/knime_4.6.4/knime -reset -nosave -consoleLog -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile=/tmp/workflow.zip "${wfvars[@]}" "${wfsecrets[@]}"
